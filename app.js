080fe0b43b6f5a9a17edb9f4c04b232057984d0b
Riley <r.mark.preslar@gmail.com>
2017-03-02 06:00:44 +0000

    I added a google map it needs to link to taco locs

--- app.js
+++ app.js
@@ -1,41 +1,55 @@
 angular.module('TacoApp', [])
-.controller('MainCtrl', ['$scope', function($scope) {
+	.controller('MainCtrl', ['$scope', function($scope) {
 
-	  // Initialize taco shops array
-	  $scope.tacoShops = [];
+		// Initialize taco shops array
+		$scope.tacoShops = [];
 
-	  $scope.generateTacoShop = function(name, address, city, state, zip, style) {
-	  	var tacoShop = {
-  			name: name,
-  			address: address,
-  			city: city,
-  			state: state,
-  			zip: zip,
-  			style: style
-	  	};
-	  	return tacoShop;
-	  }
+		$scope.generateTacoShop = function(name, address, city, state, zip, style) {
+			var tacoShop = {
+				name: name,
+				address: address,
+				city: city,
+				state: state,
+				zip: zip,
+				style: style
+			};
+			return tacoShop;
+		}
 
 		tacoShop = $scope.generateTacoShop("A shop", "1234 A Street", "Provo", "Utah", 84606, "Authentic");
-	  	$scope.tacoShops.push(tacoShop);
+		$scope.tacoShops.push(tacoShop);
 		var tacoShop = $scope.generateTacoShop("B shop", "1234 B Street", "Provo", "Utah", 84606, "TexMex");
-		$scope.tacoShops.push(tacoShop);	
+		$scope.tacoShops.push(tacoShop);
 
-      // Function for creating adding taco shops
-      // thru the UI
-	  $scope.addTacoShop = function() {
-  		var tacoShop = $scope.generateTacoShop(
-  			$scope.formName,
-  			$scope.formAddress,
-  			$scope.formCity,
-  			$scope.formState,
-  			$scope.formZip,
-  			$scope.formStyle
-		);
-  		$scope.tacoShops.push(tacoShop);
-  		console.log("About to add a new taco shop: " + tacoShop);
-  		console.log($scope.tacoShops);
-	  };	
+		// Function for creating adding taco shops
+		// thru the UI
+		$scope.addTacoShop = function() {
+			var tacoShop = $scope.generateTacoShop(
+				$scope.formName,
+				$scope.formAddress,
+				$scope.formCity,
+				$scope.formState,
+				$scope.formZip,
+				$scope.formStyle
+			);
+			$scope.tacoShops.push(tacoShop);
+			console.log("About to add a new taco shop: " + tacoShop);
+			console.log($scope.tacoShops);
+		};
 
-	}
-]);
+	}]);
+
+function initMap() {
+	var uluru = {
+		lat: 40.233845,
+		lng: -111.658531
+	};
+	var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 9,
+		center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
